9ba48bf888c7789c2eab28f97edd5ab2947918f8
Luca Santiago Latigano <lucalatigano12@gmail.com>
2026-05-06 18:29:41 -0300

    error solved with paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from "gsap/all"
 import { useRef } from "react"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
+
 export default function About({ id }) {
     const dataABout = [
         { type: "carrera", data: "Ingeniería en Informatica" },
@@ -11,10 +12,13 @@ export default function About({ id }) {
         { type: "foco", data: "Frontend · Interfaces" },
         { type: "ubicación", data: "Salta, Argentina" },
     ]
+
     const container = useRef()
     const cardContainer = useRef()
+
     useGSAP(() => {
-        const paraSplit = new SplitText(".para", { type: "lines" })
+        const paraEl = container.current.querySelector(".para")
+        const paraSplit = new SplitText(paraEl, { type: "lines" })
         const allCards = gsap.utils.toArray(".card", cardContainer.current)
         const title = container.current.querySelector(".title")
 
@@ -22,39 +26,41 @@ export default function About({ id }) {
             paraSplit.revert()
             paraSplit.split({ type: "lines" })
         }
-
         window.addEventListener("resize", handleResize)
 
-        const startItems = [title, ...paraSplit.lines]
+        gsap.set([title, ...paraSplit.lines], { opacity: 0, x: -100 })
 
-        gsap.from(startItems, {
+        ScrollTrigger.refresh()
+
+        gsap.from([title, ...paraSplit.lines], {
             opacity: 0,
             duration: 0.8,
             stagger: 0.16,
             x: -100,
             scrollTrigger: {
                 trigger: container.current,
-                start: "top 70%",
+                start: "top 85%",
                 toggleActions: "play none none none",
             },
             ease: "power2.inOut"
         })
 
         allCards.forEach(card => {
+            gsap.set(card, { opacity: 0, x: -100 })
             gsap.from(card, {
                 opacity: 0,
                 duration: 0.8,
                 x: -100,
                 scrollTrigger: {
                     trigger: card,
-                    start: "top 80%",
+                    start: "top 85%",
                     toggleActions: "play none none none",
                 },
                 ease: "power2.inOut"
             })
         })
-        return () => window.removeEventListener("resize", handleResize)
 
+        return () => window.removeEventListener("resize", handleResize)
     }, { scope: container })
 
     return (
